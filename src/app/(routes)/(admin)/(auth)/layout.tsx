22f8c160283850ead32ec85c421f4e2713feb38d
Krishna <krishna13248@gmail.com>
2024-01-09 12:19:27 +0530

    middleware removed

--- src/app/(routes)/(admin)/(auth)/layout.tsx
+++ src/app/(routes)/(admin)/(auth)/layout.tsx
@@ -1,8 +1,17 @@
-export default function AuthLayout({
+import { userAuth } from "@/lib/user-auth";
+import { redirect } from "next/navigation";
+
+export default async function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const user = await userAuth();
+
+  if (user) {
+    return redirect("/admin");
+  }
+
   return (
     <div className="h-full flex items-center justify-center">{children}</div>
   );
--- src/app/(routes)/(admin)/admin/layout.tsx
+++ src/app/(routes)/(admin)/admin/layout.tsx
@@ -1,10 +1,19 @@
+import { redirect } from "next/navigation";
+
+import { userAuth } from "@/lib/user-auth";
 import Navbar from "./_components/navbar/navbar";
 
-export default function AdminLayout({
+export default async function AdminLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const user = await userAuth();
+
+  if (!user) {
+    return redirect("/login");
+  }
+
   return (
     <div className="h-full">
       <Navbar />
